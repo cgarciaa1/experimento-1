89d554e1fcbba6a02cadb3adfd25468dcf5d1eb0
cgarciaa1 <ca.garciaa1@uniandes.edu.co>
2022-09-06 21:26:00 -0500

    no message

--- signal_processor/api_queries.py
+++ signal_processor/api_queries.py
@@ -17,16 +17,16 @@ api.add_resource(SignalProcessorResource, '/api-queries/signals')
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
 
-p = r.pubsub()
-p.subscribe("signal-channel")       
+    p = r.pubsub()
+    p.subscribe("signal-channel")       
 
-while True:
-    logging.warning("Subscrition")
-    message = p.get_message()
-    logging.warning("-------")
-    logging.warning(message)
-    logging.warning("-------")
-    if message:
-        logging.warning("Evento obtenido: {}".format(message))
-        event = message
-    time.sleep(0.01)
+    while True:
+        logging.warning("Subscrition")
+        message = p.get_message()
+        logging.warning("-------")
+        logging.warning(message)
+        logging.warning("-------")
+        if message:
+            logging.warning("Evento obtenido: {}".format(message))
+            event = message
+        time.sleep(0.01)
