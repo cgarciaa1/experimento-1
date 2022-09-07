3ba65ac00625b912124c7f1de373efcb60cb98a8
cgarciaa1 <ca.garciaa1@uniandes.edu.co>
2022-09-06 21:53:33 -0500

    no message

--- signal_processor/api_queries.py
+++ signal_processor/api_queries.py
@@ -1,32 +1,38 @@
 from base import app, api, ma, r, Resource, Flask, request
 import time
 import logging
+import threading
 
 event = ""
 
 class SignalProcessorResource(Resource):
 
     def get(self):
-        logging.warning("-------EJECUTANDO OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO")
         return {"status":"Ok", "event": event }, 200
-
-     
 
 api.add_resource(SignalProcessorResource, '/api-queries/signals')
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
 
-    p = r.pubsub()
-    p.subscribe("signal-channel")       
-    logging.warning("-------EJECUTANDO ANTES DE LOG")
+def thread_function(channel):
+    logging.warning("-------EJECUTANDO ANTES DE LOG"
+    p = r.pubsub()    
+    logging.warning("-------EJECUTANDO DESPUES DE LOG ----------"
+    p.subscribe(channel)     
+    
     while True:
-        logging.warning("Subscrition")
-        message = p.get_message()
-        logging.warning("-------")
-        logging.warning(message)
-        logging.warning("-------")
-        if message:
-            logging.warning("Evento obtenido: {}".format(message))
-            event = message
-        time.sleep(0.01)
+    logging.warning("Subscrition")
+    message = p.get_message()
+    logging.warning("-------")
+    logging.warning(message)
+    logging.warning("-------")
+    if message:
+        logging.warning("Evento obtenido: {}".format(message))
+        event = message
+    time.sleep(0.01)
+
+
+if __name__ == '__main__':
+    x = threading.Thread(target=thread_function, args=("signal-channel"))
+    x.start()
+
+    app.run(debug=True, host='0.0.0.0')
