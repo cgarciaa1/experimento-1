42d3fb3818ccf7d204f2f2e334d1df017fb90aa8
cgarciaa1 <ca.garciaa1@uniandes.edu.co>
2022-09-06 22:02:13 -0500

    no message

--- signal_processor/api_queries.py
+++ signal_processor/api_queries.py
@@ -13,26 +13,26 @@ class SignalProcessorResource(Resource):
 api.add_resource(SignalProcessorResource, '/api-queries/signals')
 
 
-def thread_function(channel):
-    logging.warning("-------EJECUTANDO ANTES DE LOG"
-    p = r.pubsub()    
-    logging.warning("-------EJECUTANDO DESPUES DE LOG ----------"
-    p.subscribe(channel)     
+def thread_function(channel, p):
     
     while True:
-    logging.warning("Subscrition")
-    message = p.get_message()
-    logging.warning("-------")
-    logging.warning(message)
-    logging.warning("-------")
-    if message:
-        logging.warning("Evento obtenido: {}".format(message))
-        event = message
-    time.sleep(0.01)
+        logging.warning("Subscrition")
+        message = p.get_message()
+        logging.warning("-------")
+        logging.warning(message)
+        logging.warning("-------")
+        if message:
+            logging.warning("Evento obtenido: {}".format(message))
+            event = message
+        time.sleep(0.01)
 
 
 if __name__ == '__main__':
-    x = threading.Thread(target=thread_function, args=("signal-channel"))
+    logging.warning("-------EJECUTANDO ANTES DE LOG"
+    p = r.pubsub()    
+    logging.warning("-------EJECUTANDO DESPUES DE LOG ----------"
+    p.subscribe(channel)   
+    x = threading.Thread(target=thread_function, args=("signal-channel", p))
     x.start()
 
     app.run(debug=True, host='0.0.0.0')
