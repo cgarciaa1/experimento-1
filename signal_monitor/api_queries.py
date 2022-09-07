8e1291bce7fd41a7a3bd780232cabcc4ed1f2e15
cgarciaa1 <ca.garciaa1@uniandes.edu.co>
2022-09-06 20:21:58 -0500

    no message

--- signal_monitor/api_queries.py
+++ signal_monitor/api_queries.py
@@ -5,8 +5,8 @@ class SignalMonitorResource(Resource):
 
     def get(self):
         print("Preparando request Monitor")
-        #response = requests.get("http://signals-queries:5000/api-queries/monitor")
-        response = "Ok-1"
+        response = requests.get("http://signals-queries:5000/api-queries/signals")
+        #response = "Ok-1"
         print("Respuesta: {}".format(response))
         return response, 200
 
